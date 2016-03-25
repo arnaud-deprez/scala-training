a20ac51ec1d228fe61781c29cf1afc2983bcf6ec
Arnaud Deprez <arnaud.deprez@lampiris.be>
2016-03-25 12:14:01 +0100

    Terminated message is the only message delivery that is guaranteed in akka

--- akka-course/src/main/scala/be/arndep/scala/akka/link/cluster/ClusterWorkerActor.scala
+++ akka-course/src/main/scala/be/arndep/scala/akka/link/cluster/ClusterWorkerActor.scala
@@ -1,6 +1,6 @@
 package be.arndep.scala.akka.link.cluster
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorIdentity, ActorLogging, Identify, RootActorPath, Terminated}
 import akka.cluster.{Cluster, ClusterEvent}
 import be.arndep.scala.akka.link.AsyncWebClient
 
@@ -14,15 +14,25 @@ import be.arndep.scala.akka.link.AsyncWebClient
 	* 	-Dakka.remote.netty.tcp.port=0
 	* 	-Dakka.cluster.autodown=on
 	*/
-class ClusterWorkerActor extends Actor{
+class ClusterWorkerActor extends Actor with ActorLogging {
 	val cluster = Cluster(context.system)
-	cluster.subscribe(self, classOf[ClusterEvent.MemberRemoved])
+	cluster.subscribe(self, classOf[ClusterEvent.MemberUp])
+//	cluster.subscribe(self, classOf[ClusterEvent.MemberRemoved])
 	val main = cluster.selfAddress.copy(port = Some(2552))
 	cluster.join(main)
 
 	def receive = {
-		case ClusterEvent.MemberRemoved(m, _) =>
-			if (m.address == main) context stop self
+		case ClusterEvent.MemberUp(member) =>
+			if (member.address == main)
+				context.actorSelection(RootActorPath(main) / "user" / "app" / "receptionist") ! Identify("42")
+		case ActorIdentity("42", None) => context stop self
+		case ActorIdentity("42", Some(ref)) =>
+			log.info("receptionist is at {}", ref)
+			context watch ref
+		//The only message that is guaranteed in akka is Terminated!
+		case Terminated(_) => context stop self
+		/*case ClusterEvent.MemberRemoved(m, _) =>
+			if (m.address == main) context stop self*/
 	}
 
 	@scala.throws[Exception](classOf[Exception])
